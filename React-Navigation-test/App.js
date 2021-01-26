9d0b0da88790cb75482e819af70a20e76f7308c1
Suriya005 <45837558+Suriya005@users.noreply.github.com>
2021-01-26 11:17:08 +0700

    Update App.js

--- React-Navigation-test/App.js
+++ React-Navigation-test/App.js
@@ -1,30 +1,46 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { Text, TextInput, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-function HomeScreen({ navigation }) {
+function HomeScreen({ navigation , route}) {
+  React.useEffect(() => {
+    if (route.params?.post){
+
+    }
+  },[route.params?.post]);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => {navigation.navigate('Details' , {
+          itemId : 86,
+          otherParam : 'anything tou want here',
+        });
+        }}
       />
       <Button
-        title="Go to About me"
-        onPress={() => navigation.navigate('About Me')}
+        title="Create post"
+        onPress={() => navigation.navigate('CreatePost')}
       />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
     </View>
   );
 }
 
-function DetailsScreen({ navigation }) {
+function DetailsScreen({route, navigation }) {
+  const {itemId , otherParam} = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Text>itemId : {JSON.stringify(itemId)}</Text>
+      <Text>otherParam : {JSON.stringify(otherParam)}</Text>
       <Button
         title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
+        onPress={() => navigation.push('Details', {
+          itemId : Math.floor(Math.random()*100),
+          otherParam : 'Random successful.'
+        })}
       />
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
       <Button title="Go back" onPress={() => navigation.goBack()} />
@@ -36,13 +52,29 @@ function DetailsScreen({ navigation }) {
   );
 }
 
-function AboutMe({ navigation }) {
+function CreatePostScreen({ route , navigation }) {
+  const [postText, setPostText] = React.useState('');
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>About me Screen</Text>
-    </View>
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass params back to home screen
+          navigation.navigate('Home', { post: postText });
+        }}
+      />
+    </>
   );
 }
+
 
 const Stack = createStackNavigator();
 
@@ -51,8 +83,8 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="About Me" component={AboutMe} />
+        <Stack.Screen name="Details" component={DetailsScreen} initialParams={{ itemId: 42 }} />
+        <Stack.Screen name="CreatePost" component={CreatePostScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
